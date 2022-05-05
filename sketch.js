a9bda5af49d18d512842946408add9b6f98d2a37
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2022-05-05 12:04:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,12 +77,56 @@ function draw() {
 
   if(gameState === PLAY){
 
-    
+    //making the hot air balloon jump
+    if(keyDown("space")) {
+      balloon.velocityY = -6 ;
+      
+    }
+
+    //adding gravity
+     balloon.velocityY = balloon.velocityY + 2;
+
+     
+    Bar();
+
+    //spawning top and bottom obstacles
+    spawnObstaclesTop();
+    spawnObstaclesBottom();
+
+//condition for END state
+if(topObstaclesGroup.isTouching(balloon) || balloon.isTouching(topGround)
+|| balloon.isTouching(bottomGround) || bottomObstaclesGroup.isTouching(balloon)){
+
+gameState = END;
+
+}
   }
 
   if(gameState === END) 
     {
+          gameOver.visible = true;
+          gameOver.depth = gameOver.depth+1
+          restart.visible = true;
+          restart.depth = restart.depth+1
+          
+          //all sprites should stop moving in the END state
+          balloon.velocityX = 0;
+          balloon.velocityY = 0;
+          topObstaclesGroup.setVelocityXEach(0);
+          bottomObstaclesGroup.setVelocityXEach(0);
+          barGroup.setVelocityXEach(0);
+  
+          //setting -1 lifetime so that obstacles don't disappear in the END state
+          topObstaclesGroup.setLifetimeEach(-1);
+          bottomObstaclesGroup.setLifetimeEach(-1);
          
+          balloon.y = 200;
+          
+          //resetting the game
+          if(mousePressedOver(restart)) 
+          {
+                reset();
+          }
 
     } 
 
@@ -92,30 +136,113 @@ function draw() {
 
 function reset()
 {
-  
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  topObstaclesGroup.destroyEach();
+  bottomObstaclesGroup.destroyEach();
+
+  score=0;
 }
 
 
 function spawnObstaclesTop() 
 {
-  
+  if(World.frameCount % 60 === 0) {
+    obstacleTop = createSprite(400,50,40,50);
+
+//obstacleTop.addImage(obsTop1);
+
+obstacleTop.scale = 0.1;
+obstacleTop.velocityX = -4;
+
+//random y positions for top obstacles
+obstacleTop.y = Math.round(random(10,100));
+
+//generate random top obstacles
+var rand = Math.round(random(1,2));
+switch(rand) {
+  case 1: obstacleTop.addImage(obsTop1);
+          break;
+  case 2: obstacleTop.addImage(obsTop2);
+          break;
+  default: break;
+}
+
+ //assign lifetime to the variable
+obstacleTop.lifetime = 100;
+
+balloon.depth = balloon.depth + 1;
+
+topObstaclesGroup.add(obstacleTop);
+
+  }
 }
 
 function spawnObstaclesBottom() 
 {
-      
+      if(World.frameCount % 60 === 0) {
+        obstacleBottom = createSprite(400,350,40,50);
+    
+    obstacleBottom.addImage(obsBottom1);
+    obstacleBottom.debug=true
+
+    
+    obstacleBottom.scale = 0.07;
+    obstacleBottom.velocityX = -4;
+    
+    
+
+   //generate random bottom obstacles
+    var rand = Math.round(random(1,3));
+    switch(rand) {
+      case 1: obstacleBottom.addImage(obsBottom1);
+              break;
+      case 2: obstacleBottom.addImage(obsBottom2);
+              break;
+      case 3: obstacleBottom.addImage(obsBottom3);
+              break;
+      default: break;
+    }
+
+     //assign lifetime to the variable
+   obstacleBottom.lifetime = 100;
+    
+   balloon.depth = balloon.depth + 1;
+
+   bottomObstaclesGroup.add(obstacleBottom);
+   
+      }
 }
 
  function Bar() 
  {
+         if(World.frameCount % 60 === 0)
+         {
+           var bar = createSprite(400,200,10,800);
+          bar.velocityX = -6
         
+          
+          bar.velocityX = -6
+          bar.depth = balloon.depth;
+          bar.lifetime = 70;
+          bar.visible = false;
+
+          barGroup.add(bar);
+         }
 }
 
 function Score()
 {
-         
+         if(balloon.isTouching(barGroup))
+         {
+           score = score + 1;
+         }
+        textFont("algerian");
+        textSize(30);
+        fill("yellow");
+        text("Score: "+ score, 250, 50);
        
   
 }
-
-  
+ 
